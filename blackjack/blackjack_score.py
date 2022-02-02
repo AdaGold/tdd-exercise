fdcf9788f11cc52922224b2cad24e1fb0a06d321
Becca Elenzil <becca@adadevelopersacademy.org>
2022-02-02 13:18:16 -0800

    Complete python solution

--- blackjack/blackjack_score.py
+++ blackjack/blackjack_score.py
@@ -1,6 +1,27 @@
-
 VALID_CARDS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King', 'Ace']
 
-def blackjack_score(hand)
+def blackjack_score(hand):
 
-end
+    score = 0
+
+    if len(hand) > 5:
+        return "Invalid"
+    
+    for card in hand:
+        if card not in VALID_CARDS:
+            return "Invalid"
+        elif card in ['Jack', 'Queen', 'King']:
+            score += 10
+        elif card == "Ace":
+            if score <= 10:
+                score += 11
+            else:
+                score += 1
+        else:
+            score += card
+
+        if score > 21:
+            return "Bust"
+
+    return score
+
